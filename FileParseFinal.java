e4c837201225a75d5068f54488dcdfe12a5aeaac
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-01 19:51:00 +0530

    Update FileParseFinal.java

--- FileParseFinal.java
+++ FileParseFinal.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.lang.NullPointerException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -45,7 +44,6 @@ public class FileParseFinal {
     {
     address add=new address();
     DocumentBuilderFactory db= DocumentBuilderFactory.newInstance();
-    db.setIgnoringElementContentWhitespace(true);
     DocumentBuilder dbld= db.newDocumentBuilder();
     Document doc = dbld.parse(f);
     doc.getDocumentElement().normalize();
