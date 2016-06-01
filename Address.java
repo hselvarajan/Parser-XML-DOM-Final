15b9b9809e97ebb948fdf4efda5eaa953e1b0ba1
Akshaya Venky <akshaya-venkatesh8@users.noreply.github.com>
2016-06-01 19:45:50 +0530

    Update and rename address.java to Address.java

--- Address.java
+++ Address.java
@@ -3,15 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fileparse1;
-
+package com.github.akshayavenkatesh8;
 import org.w3c.dom.Node;
 
 /**
  *
  * @author AKSHAYA
  */
-public class address {
+public class Address {
 
     /**
      *
